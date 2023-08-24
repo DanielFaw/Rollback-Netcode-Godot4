afd1c89f89677065cd5ed16f448c2de136046623
DanielFaw <58899301+DanielFaw@users.noreply.github.com>
2023-08-24 13:02:06 -0600

    Updated for part 9

--- explosion.gd
+++ explosion.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var despawn_timer : NetworkTimer
 @export var animation_player : NetworkAnimationPlayer
+const sound = preload("res://assets/explosion.wav")
 
 func _ready():
 	despawn_timer.timeout.connect(despawn_timer_timeout)
@@ -11,6 +12,7 @@ func _network_spawn(data: Dictionary) -> void:
 	global_position = data['position']
 	despawn_timer.start()
 	animation_player.play("Explode")
+	SyncManager.play_sound(str(get_path()) + ":explosion", sound)
 
 func despawn_timer_timeout():
 	SyncManager.despawn(self)
